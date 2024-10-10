b0c271a662379dda26ac8a8e4edae468c2a6d3ad
Ogikusa <91755872+Ogikusa@users.noreply.github.com>
2024-10-10 20:57:19 +0900

    Support checking internet connection

---
+++ lib/check-disconnection.ts
@@ -0,0 +1,51 @@
+type Status = "connected" | "disconnected" | "wating";
+
+//TODO クロージャを入れる
+let interfaceStatus: Status = "connected";
+export function checkInterfaces(networkName: string) {
+  const currentTime = new Date().toLocaleString("ja-JP");
+  const networks = Deno.networkInterfaces();
+  if (!networks.some((v) => v.name === networkName)) {
+    if (interfaceStatus === "connected") {
+      interfaceStatus = "disconnected";
+      console.log(
+        `➰ The network ${networkName} disconnected at ${currentTime} from network interfaces`
+      );
+    }
+  } else {
+    if (interfaceStatus === "disconnected") {
+      console.log(`✓ Connected Again! It's ${currentTime}`);
+      interfaceStatus = "wating";
+    }
+    if (interfaceStatus === "wating") {
+      interfaceStatus = "connected";
+    }
+  }
+}
+
+const environment = Deno.build.os;
+let internetStatus: Status = "connected";
+export function checkInternetConnection() {
+  const currentTime = new Date().toLocaleString("ja-JP");
+  if (environment === "windows") {
+    const pingCommand = new Deno.Command("ping", {
+      args: ["8.8.8.8", "-n", "1"],
+    });
+    const out = pingCommand.outputSync();
+    if (out.code !== 0) {
+      if (internetStatus === "connected") {
+        console.log(`Can't access the Internet`);
+        internetStatus = "disconnected";
+      }
+    } else {
+      if (internetStatus === "wating") {
+        internetStatus = "connected";
+      }
+      if (internetStatus === "disconnected") {
+        console.log(`✓ Able to access the Internet again! It's ${currentTime}`);
+        internetStatus = "wating";
+      }
+    }
+  }
+  //TODO: Support Linux and Darwin(Mac)
+}
